33636eaf5465ab9dcb15db75e255ac1444c0e72d
Konstantin Ulanov <GKondor@mail.ru>
2023-08-23 21:24:46 +0500

    Start def self.test_of_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+
+  @@test_cat_name = []
+
+  def self.test_of_category(category_n)
+    @@test_cat_name = Test.joins('JOIN categories on tests.category_id = categories.id').where(categories.title: category_n).order(title: :DESC).pluck(test.title)
+    @@test_cat_name
+  end  
 end
