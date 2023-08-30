45241aa03445c7d81fad4349207ee61c81f693d8
Konstantin Ulanov <GKondor@mail.ru>
2023-08-30 22:34:20 +0500

    Fix a bag

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   @@test_cat_name = []
 
   def self.test_of_category(category_name)
-    @@test_cat_name = Test.joins(:categories).where(categories: { title: category_name}).order(title: :DESC).pluck(tests.title)
+    @@test_cat_name = Test.joins('JOIN categories on tests.category_id = categories.id').where(categories: { title: category_name}).order(title: :DESC).pluck('tests.title')
     @@test_cat_name
   end  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,26 +11,26 @@ Question.destroy_all
 Answer.destroy_all
 User.destroy_all
 
-= Category.create([
+categories= Category.create!([
   { title: 'Frontend' }, 
   { title: 'Backend'}, 
   { title: 'Movie Development'}
 ]) 
-tests = Test.create([
+tests = Test.create!([
   { title: 'HTML', level: 1, category_id: categories[2].id},
   { title: 'Java', level: 3, category_id: categories[1].id},
   { title: 'GO', level: 3, category_id: categories[0].id},
   { title: 'Ruby', level: 2, category_id: categories[0].id},
   { title: 'C#', level: 2, category_id: categories[1].id}
 ])
-questions = Question.create([
+questions = Question.create!([
   { body: 'Различие между селекторами идентификаторов и классов ... ', test_id: tests[0].id},
   { body: 'В чем разница между " == " и " === "?', test_id: tests[1].id},
   { body: 'Чего нет в языке Go?', test_id: tests[2].id},
   { body: 'Назовите четыре типа переменных, доступных в классе Ruby.', test_id: tests[3].id},
   { body: 'Что такое Assembly в C#?', test_id: tests[4].id}
 ])
-answers = Answer.create([
+answers = Answer.create!([
   { body: '1', correct: true, question_id: questions[0].id },
   { body: '2', correct: true, question_id: questions[1].id },
   { body: '2', correct: true, question_id: questions[2].id },
@@ -38,4 +38,4 @@ answers = Answer.create([
   { body: '4', correct: true, question_id: questions[3].id },
   { body: '5', correct: true, question_id: questions[4].id }
 ])
-users = User.create([{ name: 'Koty'}])
+users = User.create!([{ name: 'Koty'}])
