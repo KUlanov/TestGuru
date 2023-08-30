5d67a96f4c6d8841512dbd0fc4e70f85c5c0f633
Konstantin Ulanov <GKondor@mail.ru>
2023-08-30 21:34:33 +0500

    Add find category

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
 
   @@test_cat_name = []
 
-  def self.test_of_category(category_n)
-    @@test_cat_name = Test.joins('JOIN categories on tests.category_id = categories.id').where(categories.title: category_n).order(title: :DESC).pluck(test.title)
+  def self.test_of_category(category_name)
+    @@test_cat_name = Test.joins(:categories).where(categories: { title: category_name}).order(title: :DESC).pluck(tests.title)
     @@test_cat_name
   end  
 end
