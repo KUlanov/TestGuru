8d5aa59f0dea85482bb8b9f8737e7fc847a6c976
Konstantin Ulanov <GKondor@mail.ru>
2023-09-01 21:58:42 +0500

    Added connections

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,11 @@
 class Test < ApplicationRecord
+  has_many :usertests
+  has_many :users, through: :usertests
 
   @@test_cat_name = []
 
   def self.test_of_category(category_name)
     @@test_cat_name = Test.joins('JOIN categories on tests.category_id = categories.id').where(categories: { title: category_name}).order(title: :DESC).pluck('tests.title')
     @@test_cat_name
-  end  
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :usertests
+  has_many :tests, through: :usertests
+
+  def user_level_test(level)
+
+  end
 end
