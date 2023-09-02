0f163340fca4eb2f8beba43b00a7b7f85789c37e
Konstantin Ulanov <GKondor@mail.ru>
2023-09-02 15:14:10 +0500

    added links to the database

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
-  has_many :usertests
-  has_many :users, through: :usertests
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  has_many :questions
+  belongs_to :category
+  belongs_to :user
   
   def self.test_of_category(category_name)
     Test.joins('JOIN categories on tests.category_id = categories.id').where(categories: { title: category_name}).order(title: :DESC).pluck('tests.title')    
--- db/migrate/20230810152231_create_tests.rb
+++ db/migrate/20230810152231_create_tests.rb
@@ -2,6 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.belongs_to :category, foreign_key: true
+      t.belongs_to :user, foreign_key: true
       t.string :title, null: false
       t.integer :level, default: 0
       
