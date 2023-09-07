9c3cd82e413af7e0c2a39533d03fae0acc3a4d2d
Konstantin Ulanov <GKondor@mail.ru>
2023-09-07 22:06:11 +0500

    Start add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
   has_many :authors_tests, inverse_of: 'author'
 
+  
+
   def user_level_test(level)
     tests.where(tests: {level: level})
   end
