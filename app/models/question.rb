e7f5d11f578bbedd2b6e92561cb9492c866db80a
Konstantin Ulanov <GKondor@mail.ru>
2023-09-07 19:52:57 +0500

    Add scope

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers
   belongs_to :test
+
+  scope :correct_answers, -> {answers.where(correct: true) }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,9 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   
-  def self.test_of_category(category_name)
-    Category.find_by(title: category_name).tests.order(title: :DESC).pluck(:title)
-  end
+  scope :test_of_category, -> (category_name) {Category.find_by(title: category_name).tests.order(title: :DESC)}
+
+  scope :easy_tests, -> {where(level: 0..1)}
+  scope :medium_tests, -> {where(level: 2..4)}
+  scope :hard_tests, -> {where(level: 5..Float::INFINITY)}
 end
