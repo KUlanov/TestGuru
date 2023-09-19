ffa8c58c3db4339afb270c4820764c9b4617956f
Konstantin Ulanov <GKondor@mail.ru>
2023-09-19 20:29:15 +0500

    Add tests create

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,4 +4,13 @@ class TestsController < ApplicationController
     #render html:  '<h1>All tests</h1>'.html_safe
     render json: { tests: Test.all }
   end
+  
+  def new
+  end
+  
+  def create
+    result = ["Class: #{params.class}","Parameters: #{params.inspect}"]
+  
+    render plain: result.join("\n")
+  end
 end
