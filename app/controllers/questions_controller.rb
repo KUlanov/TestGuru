58e2b5a39016fe8ba0233300463cf981ea355e09
Konstantin Ulanov <GKondor@mail.ru>
2023-09-21 21:10:04 +0500

    Leson7 v1.1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,15 @@
 class QuestionsController < ApplicationController
 
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  
   def index
-    render json: { questions: Question.all }
+    render plain: @tests.questions.pluck(:id, :body)
+  end
+
+  def show
+    render plain: @question.body
   end
 
   def new
@@ -9,6 +17,32 @@ class QuestionsController < ApplicationController
   end
 
   def create
-
+    question = @tests.questions.new(question_params)
+    question.save!
+    render plain: question.inspect
   end
+
+  def destroy
+    @question.destroy!
+    render plain 'Question destroy.'
+  end
+
+private
+
+  def rescue_with_question_not_found
+    render plain: 'Question not faund'
+  end
+
+  def find_test
+    @tests = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
 end
