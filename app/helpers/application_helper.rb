e518cfafbdf571bf8673160bd6948bf7b6cba159
Konstantin Ulanov <GKondor@mail.ru>
2023-10-17 18:28:29 +0500

    Add footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    Time.zone.now.year
+  end
+
+  def github_ulr(author, repo)
+    link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
