b79b48f776bad8ee385273727c70a122b1d882e4
Konstantin <GKondor@mail.ru>
2023-10-24 15:58:34 +0500

    Pull recuests ver 1.1

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+    def question_header(question)
+      if question.persisted?
+        "Edit #{question.test.title} "
+      else
+        "Create new #{question.test.title} "
+      end
+    end
 end
