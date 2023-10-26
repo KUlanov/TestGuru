4dad98e73fa318559ecb1304cbcc87a74422523e
Konstantin <GKondor@mail.ru>
2023-10-26 16:03:42 +0500

    26/10

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,15 +1,2 @@
 class Answer < ApplicationRecord
-  belongs_to :question
-
-  validates :body, presence: true
-  
-  validate :validate_answers_count
-
-  scope :correct_answers, -> {where(correct: true) }
-
-  private
-
-  def validate_answers_count
-    errors.add(:answers_count) if question.answers.count > 4
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :tests do
-    resources :questions, shallow: true
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'tests#index'
 
-  root to: 'tests#index'
+  resources :tests do
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
+  end
+
 end
