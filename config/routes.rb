722e4857bd76d62d1692bfe0163cca3b2f83f324
Konstantin <GKondor@mail.ru>
2023-10-27 16:20:38 +0500

    27/10

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'tests#index'
 
   resources :tests do
-    resources :questions, shallow: true, except: :index do
+    resources :questions, shallow: true do
       resources :answers, shallow: true
     end
   end
