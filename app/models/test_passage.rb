db0671908738ddcfe6cbaaf0e06c99f281aec543
Konstantin Ulanov <GKondor@mail.ru>
2023-11-11 19:10:47 +0500

    Pull 9 v1.0

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,10 +4,62 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  after_validation :after_validation_set_next_question, on: :update
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+    save!
+  end
+
+  def completed?
+    current_question.nil?
+  end
+
+  def questions_count
+    test_passage_questions.count
+  end
+
+  def questions_index
+    test_passage_questions.index(current_question)+1
+  end
+
+  def percent
+    (correct_questions.to_f/questions_count)*100
+  end
+
+  def succesfully?
+    percent >= 85
+  end
+
 
   private
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
   end
+
+  def correct_answer?(answer_ids)
+    correct_answers_count = correct_answers.count
+    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
+    correct_answers_count == answer_ids.count
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
+  def after_validation_set_next_question
+    self.current_question = next_question
+  end
+
+  def test_passage_questions
+    self.test.questions
+  end
+
 end
