b8d2dbc0c845ea226e4c4d0e7aaf24d3a8b61c78
Konstantin <GKondor@mail.ru>
2024-01-17 16:26:08 +0500

    At work 17/01

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :expection
   
   before_action :authenticate_user!
-  before_action :sign_for_user_path()
+  #before_action :admin_path_redirect
   
 private
-  
+
+  def admin_path_redirect
+    redirect_to admin_tests_path if current_user.type = 'Admin'
+  end
+
 end
