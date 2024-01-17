cbd39b27421e4b1ff25a5f3a43d07d4e54b55144
Konstantin Ulanov <GKondor@mail.ru>
2024-01-18 00:08:50 +0500

    Task v1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,15 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :expection
   
   before_action :authenticate_user!
-  #before_action :admin_path_redirect
   
 private
 
-  def admin_path_redirect
-    redirect_to admin_tests_path if current_user.type = 'Admin'
+  def after_sign_in_path_for(user)
+    if user.type = 'admin'
+      admin_tests_path
+    else
+      user_root_path
+    end
   end
 
 end
